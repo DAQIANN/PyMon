e5818646c899724b71a4352dfeb050fbb23c852a
Daniel Qian <danielqian2015@gmail.com>
2020-09-14 21:18:19 -0500

    Went through finding with query

--- BasicPyMon/queryMDB.py
+++ BasicPyMon/queryMDB.py
@@ -4,15 +4,27 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 
-myquery = {"address":"Park Lane 38"}
-#mydoc = mycol.find(myquery)
+def firstquery():
+    myquery = {"address":"Park Lane 38"}
+    mydoc = mycol.find(myquery)
 
-#for x in mydoc:
-    #print(x)
+    for x in mydoc:
+        print(x)
 
-#find address starting with letter "S" or higher
-newquery = {"address":{"$gt":"S"}}
-newdoc = mycol.find(newquery)
+def newquery():
+    #find address starting with letter "S" or higher
+    newquery = {"address":{"$gt":"S"}}
+    newdoc = mycol.find(newquery)
 
-for y in newdoc:
-    print(y)
+    for y in newdoc:
+        print(y)
+
+def regexpress():
+    #Starts with "S"
+    myquery = {"address":{"$regex":"^S"}}
+    mydoc = mycol.find(myquery)
+
+    for x in mydoc:
+        print(x)
+
+regexpress()
