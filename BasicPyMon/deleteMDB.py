5e8e93233cd6b1644c2f85db9bfcd2dab0664af8
Daniel Qian <danielqian2015@gmail.com>
2020-09-14 22:50:26 -0500

    Created delete functions

---
+++ BasicPyMon/deleteMDB.py
@@ -0,0 +1,20 @@
+import pymongo
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
+
+def singledelete():
+    myquery = {"address":"Mountain 21"}
+    mycol.delete_one(myquery)
+
+def manydelete():
+    myquery = {"address":{"regex":"^S"}}
+    x = mycol.delete_many(myquery)
+    print(x.deleted_count, " documents deleted.")
+
+def alldelete():
+    x = mycol.delete_many({})
+    print(x.deleted_count, " documents deleted.")
+
+alldelete()
